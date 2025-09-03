b042e30870005d4d46f2350cfed97ac687da8ce2
Eyob Worku <eyobworku62@gmail.com>
2025-09-03 09:26:00 +0300

    Created 'Most Stones Removed with Same Row or Column' by A2SV Hub

---
+++ 03-Sep-2025/Most Stones Removed with Same Row or Column 391034.py
@@ -0,0 +1,38 @@
+# Problem: Most Stones Removed with Same Row or Column - https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/
+
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.size = [1] * size
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.size[rootX] > self.size[rootY]:
+                self.root[rootY] = rootX
+                self.size[rootX] += self.size[rootY]
+            else:
+                self.root[rootX] = rootY
+                self.size[rootY] += self.size[rootX]
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        n =len(stones)
+        dsu = UnionFind(n)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if stones[i][0] == stones[j][0] or stones[i][1] == stones[j][1]:
+                    dsu.union(i, j)
+        res = set()
+        for i in range(n):
+            res.add(dsu.find(i))
+        unremoved = 0
+        for x in res:
+            unremoved+=dsu.size[x]-1
+        return unremoved
+
