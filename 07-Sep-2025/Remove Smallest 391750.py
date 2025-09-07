8666e7c98447983f6e8fd9d184882aaffa59e054
Eyob Worku <eyobworku62@gmail.com>
2025-09-07 11:24:14 +0300

    Created 'Remove Smallest' by A2SV Hub

---
+++ 07-Sep-2025/Remove Smallest 391750.py
@@ -0,0 +1,14 @@
+# Problem: Remove Smallest - https://codeforces.com/problemset/problem/1399/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int,input().split()))
+    a.sort()
+    res = True
+    if n > 1:
+        for i in range(n-1):
+            if a[i+1]-a[i] > 1:
+                res = False
+                break
+    print("YES" if res else "NO")
