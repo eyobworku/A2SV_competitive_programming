8081145f671766649a2a609f3fb4f9f7038af467
Eyob Worku <eyobworku62@gmail.com>
2025-09-11 10:25:48 +0300

    Created 'Find Greatest Common Divisor of Array' by A2SV Hub

---
+++ 11-Sep-2025/Find Greatest Common Divisor of Array 392556.py
@@ -0,0 +1,11 @@
+# Problem: Find Greatest Common Divisor of Array - https://leetcode.com/problems/find-greatest-common-divisor-of-array/
+
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+        mn = min(nums)
+        mx = max(nums)
+        res = 1
+        for x in range(1,mn+1):
+            if mn%x == 0 and mx%x == 0:
+                res =x
+        return res
