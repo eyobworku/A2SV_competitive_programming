8dcdd9aef38c0d4a06a704161a3f7a3a3f0c4032
Eyob Worku <eyobworku62@gmail.com>
2025-09-30 20:42:12 +0300

    Created 'Partition String ' by A2SV Hub

---
+++ 30-Sep-2025/Partition String  398090.py
@@ -0,0 +1,39 @@
+# Problem: Partition String  - https://leetcode.com/problems/partition-string/description/
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word_end = 0
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.word_end += 1
+
+    def check(self, word):
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return node.word_end != 0
+
+class Solution:
+    def partitionString(self, s: str) -> List[str]:
+        trie = Trie()
+        word = ''
+        segments = []
+        for l in s:
+            word += l
+            if not trie.check(word):
+                trie.insert(word)
+                segments.append(word)
+                word = ''
+        return segments
