d530ffde9ec8daad92375a5bee6b6618ed4f6ff5
Eyob Worku <eyobworku62@gmail.com>
2025-10-02 12:08:02 +0300

    Created 'Short Encoding of Words' by A2SV Hub

---
+++ 02-Oct-2025/Short Encoding of Words 398893.py
@@ -0,0 +1,28 @@
+# Problem: Short Encoding of Words - https://leetcode.com/problems/short-encoding-of-words/
+
+class TrieNode:
+    def __init__(self, ch: Optional[str]='#'):
+        self.ch = ch
+        self.cd = {}
+        self.end = False
+        
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for i in range(len(word)):
+            if word[~i] not in curr.cd:
+                curr.cd[word[~i]] = TrieNode(word[~i])
+            curr = curr.cd[word[~i]]
+        curr.end = True
+
+class Solution:
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+        trie = Trie()
+        for word in words:
+            trie.insert(word)
+        def dfs(node: TrieNode, curr: int) -> int:
+            return sum(dfs(adj, curr+1) for adj in node.cd.values()) if node.cd else curr
+        return dfs(trie.root, 1)
